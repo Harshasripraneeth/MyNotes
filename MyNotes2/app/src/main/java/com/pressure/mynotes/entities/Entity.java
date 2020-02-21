7312d3bad16aa0cc31432e0482d444a723b48f75
Harshasripraneeth <46346478+Harshasripraneeth@users.noreply.github.com>
2020-02-21 13:11:29 +0530

    Entity for the database

---
+++ MyNotes2/app/src/main/java/com/pressure/mynotes/entities/Entity.java
@@ -0,0 +1,52 @@
+package com.pressure.mynotes.entities;
+
+
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@androidx.room.Entity(tableName = "MyNotes")
+public class Entity {
+    @PrimaryKey(autoGenerate = true)
+    int id;
+    String title;
+    String content;
+
+    public Entity(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+    @Ignore
+    public Entity( String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+
+}
