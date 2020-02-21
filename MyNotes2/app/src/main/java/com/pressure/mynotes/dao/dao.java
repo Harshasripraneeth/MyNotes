eb55e6ce729b2d6e4e706de64c90e452e46f3b03
Harshasripraneeth <46346478+Harshasripraneeth@users.noreply.github.com>
2020-02-21 13:11:02 +0530

    no message

---
+++ MyNotes2/app/src/main/java/com/pressure/mynotes/dao/dao.java
@@ -0,0 +1,36 @@
+package com.pressure.mynotes.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.pressure.mynotes.entities.Entity;
+
+import java.util.List;
+
+@Dao
+public interface dao {
+
+
+    @Query("SELECT * FROM MyNotes")
+    List<Entity> loadAllTasks();
+    @Insert
+    void insertTask(com.pressure.mynotes.entities.Entity taskEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateTask(com.pressure.mynotes.entities.Entity taskEntry);
+
+    @Delete
+    void deleteTask(com.pressure.mynotes.entities.Entity taskEntry);
+
+    @Query("SELECT * FROM MyNotes WHERE id LIKE :id")
+    Entity loadById(int id);
+    @Query("SELECT * FROM mynotes WHERE title LIKE :text OR content LIKE :text")
+    List<Entity> loadUpdatedList(String text);
+
+    @Query("DELETE FROM mynotes")
+     void deleteAllNotes();
+   }
